6377b58fcc247b5f77e19b1c17b79275f081f772
Xavier RENE-CORAIL <xavier.renecorail@gmail.com>
2020-03-03 13:23:34 -0800

    Add publish script

---
+++ templates/action/image/publish.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+set -x
+
+docker login docker.pkg.github.com -u github-actions -p ${GITHUB_TOKEN}
+
+IMAGE_VERSION=latest
+IMAGE_TAG=docker.pkg.github.com/github/codeql-learninglab-actions/courses-cpp-<course-name>:${IMAGE_VERSION}
+
+docker build -t $IMAGE_TAG .
+
+docker push $IMAGE_TAG
