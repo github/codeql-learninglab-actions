15e5cf5e4aed2e955d3b9f165726a9ed6bb845aa
Xavier RENE-CORAIL <xavier.renecorail@gmail.com>
2020-04-06 15:49:20 -0700

    consistency

--- templates/action/image/publish.sh
+++ templates/action/image/publish.sh
@@ -7,9 +7,9 @@ docker login docker.pkg.github.com -u github-actions -p ${GITHUB_TOKEN}
 
 IMAGE_VERSION=0.0.1
 IMAGE_TAG=docker.pkg.github.com/github/codeql-learninglab-actions/courses-cpp-<course-name>:${IMAGE_VERSION}
-IMAGE_TAG_LATEST=docker.pkg.github.com/github/codeql-learninglab-actions/courses-cpp-<course-name>:latest
+IMAGE_LATEST_TAG=docker.pkg.github.com/github/codeql-learninglab-actions/courses-cpp-<course-name>:latest
 
-docker build -t $IMAGE_TAG -t $IMAGE_TAG_LATEST .
+docker build -t $IMAGE_TAG -t $IMAGE_LATEST_TAG .
 
 docker push $IMAGE_TAG
-docker push $IMAGE_TAG_LATEST
+docker push $IMAGE_LATEST_TAG
