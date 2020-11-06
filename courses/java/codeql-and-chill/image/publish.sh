95c6d44ed73c104a29e825a1a463440ece288b3c
Xavier Rene-Corail <xcorail@github.com>
2020-11-05 16:08:24 -0800

    init course

---
+++ courses/java/codeql-and-chill/image/publish.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -e
+set -x
+
+docker login docker.pkg.github.com -u github-actions -p ${GITHUB_TOKEN}
+
+IMAGE_VERSION=0.0.1
+IMAGE_TAG=docker.pkg.github.com/github/codeql-learninglab-actions/courses-java-codeql-and-chill:${IMAGE_VERSION}
+IMAGE_LATEST_TAG=docker.pkg.github.com/github/codeql-learninglab-actions/courses-java-codeql-and-chill:latest
+
+docker build -t $IMAGE_TAG -t $IMAGE_LATEST_TAG .
+
+docker push $IMAGE_TAG
+docker push $IMAGE_LATEST_TAG
